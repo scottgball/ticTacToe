1578f9168cd0d22ca2b9c5b17154374b52bf45f2
Scott Ball <scottgball@gmail.com>
2021-12-26 19:05:20 -0800

    Added turn-taking functionality

--- script.js
+++ script.js
@@ -1,38 +1,5 @@
-const gameBoard = (function() {
-
-  const boardSpaces = document.querySelectorAll(".boardSpace");
-
-  const _spaceContents = ['', '', '', '', '', '', '', '', ''];
-  
-  const updateArr = () => {
-    for (let i = 0; i <= _spaceContents.length; i++) {
-      _spaceContents[i] = boardSpaces[i].textContent;
-    };
-  };
-
-  const markSpace = () => {
-    boardSpaces.forEach((boardSpace) => {
-      boardSpace.addEventListener('click', (e) => {
-        boardSpace.textContent = playerDetails.playerTwo.playerMarker;
-        updateArr();
-      });
-    });
-  }
-
-  return {
-    markSpace,
-    _spaceContents
-  };
-  
-  
-})();
-
-const displayController = (function() {
-
-  
-})();
-
 const playerDetails = (function() {
+
   const playerFactory = (playerName, playerMarker) => {
     const sayHello = () => console.log(`Hello ${playerName}, you're playing as ${playerMarker}`);
     return {
@@ -41,6 +8,8 @@ const playerDetails = (function() {
       sayHello
     };
   };
+  
+  
 
   const playerOneName = prompt('Hello Player One, please enter your name');
   const playerTwoName = prompt('Hello Player Two, please enter your name');
@@ -49,6 +18,9 @@ const playerDetails = (function() {
   const playerOne = playerFactory(playerOneName, 'X');
   const playerTwo = playerFactory(playerTwoName, 'O');
 
+  
+
+
   return {
     playerOne,
     playerTwo,
@@ -56,6 +28,73 @@ const playerDetails = (function() {
 })();
 
 
-gameBoard.markSpace();
+const gameBoard = (function() {
 
-// const Scott = playerFactory('Scott', 'x');
+  
+  const boardSpaces = document.querySelectorAll(".boardSpace");
+
+  let currentPlayerMarker = playerDetails.playerTwo.playerMarker;
+
+  
+
+  const toggleCurrentPlayer = () => {
+    if (currentPlayerMarker === playerDetails.playerOne.playerMarker) {
+      return currentPlayerMarker = playerDetails.playerTwo.playerMarker;
+    } else if (currentPlayerMarker === playerDetails.playerTwo.playerMarker) {
+      return currentPlayerMarker = playerDetails.playerOne.playerMarker;
+    };
+ };
+
+  boardSpaces.forEach((boardSpace) => {
+    boardSpace.addEventListener('click', (e) => {
+      boardSpace.textContent = currentPlayerMarker;
+      toggleCurrentPlayer();
+      updateArr();
+      
+    });
+  });
+
+  const _spaceContents = ['', '', '', '', '', '', '', '', ''];
+  
+  const updateArr = () => {
+    for (let i = 0; i < _spaceContents.length; i++)
+      _spaceContents[i] = boardSpaces[i].textContent;
+  };
+
+  return {
+    updateArr,
+    _spaceContents
+  };
+  
+  
+})();
+
+const gameController = (function() {
+
+  
+  
+  
+  
+
+  // playerOneSelect();
+  // updateArr();
+  // playerTwoSelect();
+  // updateArr();
+  
+
+  
+
+  
+
+  
+
+  return {
+  
+  
+    
+  };
+})();
+
+
+
+
